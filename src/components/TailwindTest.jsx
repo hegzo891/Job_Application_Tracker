ca2ed9e8eac6201d5d234b9238c53effbf4e555e
ahmed hegazy <121024792+hegzo891@users.noreply.github.com>
2025-07-30 02:45:45 +0300

    project initialize

---
+++ src/components/TailwindTest.jsx
@@ -0,0 +1,8 @@
+// src/components/TailwindTest.jsx
+export default function TailwindTest() {
+  return (
+    <div className="bg-green-500 text-white p-4 rounded-lg">
+      Tailwind is working if this is green!
+    </div>
+  )
+}
